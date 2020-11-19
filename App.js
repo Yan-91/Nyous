e391f07a50a9709c56994e3b62192d711ba2b82b
Yan Ad'Vincula Trindade <61596255+Yan-91@users.noreply.github.com>
2020-11-19 13:19:11 -0300

    inicio

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
+
+const Drawer = createDrawerNavigator();
+
+const Autenticado = () => {
+  return(
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home-" component={Home} />
+        <Drawer.Screen name="Logout" component={Logout} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Autenticado" component={Autenticado} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
